a8b194dc6383758d47ecb2df67349b895e77282c
Yogesh Tripathi <yogeshtripathi14102003@gmail.com>
2026-04-11 17:57:46 +0530

    env ch

--- careervidyanext/src/app/page.js
+++ careervidyanext/src/app/page.js
@@ -35,6 +35,7 @@ export default function Home() {
      
    <QueryPopup />  
       <Studentimageslider />
+      
          <LogoSlider />
           <TestimonialsSlider />
          {/* <CardSlider /> */}
