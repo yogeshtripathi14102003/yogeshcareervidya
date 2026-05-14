e10bdd813770cfd9ea1dd30b1f4b97d297653107
Yogesh Tripathi <yogeshtripathi14102003@gmail.com>
2026-05-14 18:21:29 +0530

    serverfetch

--- careervidyanext/src/utlis/serverFetch.js
+++ careervidyanext/src/utlis/serverFetch.js
@@ -1,11 +1,23 @@
+// // src/utlis/serverFetch.js
+
+// export const serverFetch = async (path, options = {}) => {
+//   const baseURL = process.env.API_URL;
+  
+//   const res = await fetch(`${baseURL}${path}`, {
+//     ...options,
+//   });
+  
+//   return res;
+// }; 
+
 // src/utlis/serverFetch.js
 
 export const serverFetch = async (path, options = {}) => {
-  const baseURL = process.env.API_URL;
-  
+  const baseURL = process.env.API_URL || "https://api.careervidya.in";
+
   const res = await fetch(`${baseURL}${path}`, {
     ...options,
   });
-  
+
   return res;
-}; 
+};
