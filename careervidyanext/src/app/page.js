283dfa53063b26755bb9e8d4dd10f4185fcdc4c9
Yogesh Tripathi <yogeshtripathi14102003@gmail.com>
2026-08-15 20:10:12 +0530

    all

--- careervidyanext/src/app/page.js
+++ careervidyanext/src/app/page.js
@@ -910,26 +910,28 @@
 // }
 
 
-
-import Header from "../layout/Header";
-import Footer from "../layout/Footer";
-import Slider from "../components/slider/page";
-import Counter from "../components/counter/page";
-import Getcourse from "../components/Getcourse";
-import Universityimage from "../components/universityimage";
-import FLOW from "../components/FLOW";
-import Studentstrustus from "../components/Studentstrustus";
-import TeamSection from "../components/TeamSection";
-import TestimonialsSlider from "../components/TestimonialsSlider";
-import Studentimageslider from "../components/Studentimageslider";
-import LogoSlider from "../components/LogoSlider";
-import ServiceSlider from "../components/ServiceSlider";
-import QueryPopup from "../components/QueryPopup";
-import VideoSlider from "../components/VideoSlider";
-import FAQ from "../components/FAQ";
-import ChatbotFloating from "../components/ChatbotFloating";
-import SocialSidebar from "../components/SocialSidebar";
-import Notification from "../components/Notification";
+  
+// import CardSlider from "./components/cardslider/page";
+import Counter from "./components/counter/page";
+import Slider from "./components/slider/page";
+import TeamSection from "./components/TeamSection";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import Studentimageslider from "./components/Studentimageslider";
+import Getcourse from "./components/Getcourse";
+import FAQ from "./components/FAQ";
+import FLOW from "./components/FLOW";
+import Studentstrustus from "./components/Studentstrustus";
+import TestimonialsSlider from "./components/TestimonialsSlider";
+import LogoSlider from "./components/LogoSlider";
+  import QueryPopup from "./components/QueryPopup";
+import  Universityimage  from "../app/components/universityimage";
+import ChatbotFloating from "./components/ChatbotFloating";
+import SocialSidebar from "./components/SocialSidebar";
+import ServiceSlider from "./components/ServiceSlider";
+import Notification from "../app/components/Notification";
+import VideoSlider from "./components/VideoSlider";
+//  import CareervidyaFormModal from "./components/CareervidyaFormModal";
 
 // ✅ Forces this page to render on every request (SSR) instead of being
 // pre-rendered at build time. Needed because HeroSlider/LogoSlider fetch
